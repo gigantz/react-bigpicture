1be1d97cd8bd689d12764772e94a49ca2d1d6f6b
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2018-11-10 13:12:53 +0400

    fixed path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import BigPicture from './index.js';
+import BigPicture from './bigpicture.js';
 
 class BigPictureWrapper extends React.Component {
   static defaultProps = {
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
   },
   plugins: [
     new webpack.DefinePlugin({
